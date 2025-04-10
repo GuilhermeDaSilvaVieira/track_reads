aad94328e9a0602f0ada9c7b2ddfd1cc4c42380f
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-04-10 10:47:57 -0300

    feat(auth): use equatable for value based equality

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -1,9 +1,14 @@
-class User {
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
   final String uid;
   final String email;
 
-  User({
+  const User({
     required this.uid,
     required this.email,
   });
+
+  @override
+  List<Object?> get props => [uid, email];
 }
